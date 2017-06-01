692a9d97868a0d78a27e8fa38160d9d0666cdc41
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-06-01 17:00:27 +0200

    force redirect to https

--- app.js
+++ app.js
@@ -23,6 +23,24 @@ dotenv.load();
 
 mongoose.connect(process.env.MONGODB_URI);
 
+
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
